25bfe8f3f8b162c359e51eec8912b465278591fc
Iurymartins46 <martinsiury46@gmail.com>
2025-04-07 21:06:19 -0300

    :sparkles: feat: Adicionando execução como migration

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,15 +1,23 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
 
-export const typeOrmConfig = async (): Promise<TypeOrmModuleOptions> => ({
+dotenv.config(); 
+
+const options: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432', 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
   logging: process.env.DB_LOGGING === 'true',
   entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
-});
+  migrations: [join(__dirname, '..', 'migrations', '*.{ts,js}')],
+};
+
+export const typeOrmConfig = async (): Promise<TypeOrmModuleOptions> => options;
+
+export const AppDataSource = new DataSource(options);
